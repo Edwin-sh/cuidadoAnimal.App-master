835e1896bd2d71cb5744e642a6fbfe12629cdce7
Edwin Martínez <edwinmartinez11032003@gmail.com>
2022-09-14 19:14:05 -0500

    Updating

---
+++ cuidadoAnimal.App-master/cuidadoAnimal.App.consola2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using cuidadoAnimal.App.dominio;
+using cuidadoAnimal.App.persistencia;
+
+namespace cuidadoAnimal.App.consola2
+{
+   class Program
+
+   {
+    private static IrepositorioPropietario _repoPropietario=new RepositorioPropietario(new persistencia.AppContext());
+    
+    static void Main(string[] args)
+    {
+     Console.WriteLine("HOLA MUNDO");
+     //AddPropietario();
+     BuscarPropietario(4);
+    }
+    
+    private static void AddPropietario()
+    {
+      var propietario=new Propietario()
+      {
+        // IdPersona=11,
+         Nombres="David",
+         Apellidos="ospina",
+         Telefono="400404",
+         NombreHacienda=3333,
+         Email="ospi@ww",
+         Direccion="av 456"
+      };  
+      _repoPropietario.AddPropietario(propietario);
+
+    } 
+    private static void BuscarPropietario(int idcliente)
+    {
+      var propietario=_repoPropietario.GetPropietario(idcliente);
+      Console.WriteLine(propietario.Nombres);
+
+    }
+
+   } 
+}
